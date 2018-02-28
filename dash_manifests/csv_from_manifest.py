b52889f7f8562f8810dc6a7d84269d6326ca07b7
tehlinger <ehlinger.thibaut@gmail.com>
2018-02-28 15:49:25 +0100

    Manifest ready, must still implement to unimpoeented funcitons

---
+++ dash_manifests/csv_from_manifest.py
@@ -0,0 +1,49 @@
+import re
+
+ID = "oFkulzWMotY"
+
+#249          webm       audio only DASH audio   52k , opus @ 50k, 355.54KiB
+#242          webm       426x240    240p  292k , vp9, 24fps, video only, 1.67MiB
+
+def generate_csv(video_id=ID):
+    with open(video_id+".manifest","r") as f,open(video_id+'.csv',"w") as out:
+        lines = f.readlines()
+        for l in lines:
+            if is_format_line(l):
+                s = one_space_only(l)
+                if 'audio' in s:
+                    l = get_fields_from_audio(s)
+                else:
+                    if len(l.split(',')) > 3:
+                        l = get_fields_from_video(s)
+                    else:
+                        continue
+                out.write(','.join(l)+"\n")
+
+
+def get_fields_from_audio(line):
+    description = line.split(',')[0]
+    description = description.replace(" audio only DASH audio"," audio ")
+    description = one_space_only(description)
+    desc_list = description.split(" ")
+    codec_name = line.split(',')[1]
+    desc_list.append(codec_name.replace(" ",""))
+    desc_list.append('')
+    return desc_list
+
+def get_fields_from_video(line):
+    description = line.split(',')[0]
+    fields_list = description.split(' ')[:3]
+    fields_list.append(description.split(' ')[4])
+    fields_list.append(line.split(',')[1].replace(" ",""))
+    fields_list.append(line.split(',')[2].replace(" ",""))
+    return fields_list
+
+
+def is_format_line(l):
+    return l[:2].isdigit() and ',' in l
+
+def one_space_only(l):
+    return ' '.join(l.split())
+
+generate_csv()
---
+++ master/codec_getter.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+def prepare_json_for_mos(res_dic,video_ID="oFkulzWMotY"):
+    return {}
+
+class codecGetter:
+    def __init__(self,video_ID):
+        self.df = pd.read_csv("../dash_manifests/"+video_ID+".csv")
+
+    def get(self,codec_id):
+        """Returns the codec dic matching the codec_id"""
+        entry = self.df.loc[self.df.id == int(codec_id)].iloc[0].to_dict()
+        #If the codec is a video codec
+        if 'audio' not in entry['resolution']:
+            print(entry['resolution'])
+            name = "h264" if "avc" in entry['codec'] else "vp9"
+            res  = entry['resolution']
+            bitrate = entry['bitrate']
+            print(entry['fps'][:-3])
+            fps = int(entry['fps'][:-3])
+            return {'codec':name, 'res':res,
+                    'bitrate':bitrate,'fps':fps}
+        #If the codec is an audio codec
+        else:
+            name = "heaac" if "opus" in entry['codec'] else "aaclc"
+            return {"codec":name,"bitrate":entry['bitrate']}
